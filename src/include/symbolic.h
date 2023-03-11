282bea041555c8786fd442f04e99db643fe3e202
Uri Dvir <uri.j.dvir@gmail.com>
2023-03-11 18:43:00 -0500

    Started symbolic.h

--- src/include/symbolic.h
+++ src/include/symbolic.h
@@ -1,10 +1,46 @@
 namespace symbolic {
 
-class RealSymbol {};
-
-class RealToRealFunction {
+class Function {
     public:
-    float operator()(float x) const;
+    virtual float operator()(float x) const;
 };
+
+class IdentityFunction : public Function {
+    public:
+    IdentityFunction() {}
+    float operator()(float x) const override { return x; }
+};
+
+class ConstantFunction : public Function {
+    private:
+    const float C;
+
+    public:
+    ConstantFunction(float C) : C(C) {};
+    float operator()(float x) const override { return C; }
+};
+
+class SumFunction : public Function {
+    private:
+    const Function& lhs;
+    const Function& rhs;
+
+    public:
+    SumFunction(const Function& lhs, const Function& rhs) : lhs(lhs), rhs(rhs) {}
+    float operator()(float x) const override { return lhs(x) + rhs(x); }
+};
+
+class Symbol {
+    public:
+    operator Function() const { return IdentityFunction(); };
+};
+
+SumFunction& operator+(const Function& lhs, const float rhs){
+    return SumFunction(lhs, ConstantFunction(rhs));
+}
+
+SumFunction& operator+(const Symbol& lhs, const float rhs){
+    return IdentityFunction() + rhs;
+}
 
 }
--- test/RealFunctionTest.cpp
+++ test/RealFunctionTest.cpp
@@ -5,9 +5,9 @@ using namespace symbolic;
 
 int main(int argc, char* argv[]){
 
-    RealSymbol x;
+    Symbol x;
 
-    auto f = x + 2;
+    Function f = x + 2;
 
     std::cout << "f(x) = x + 2; f(4) = " << f(4) << std::endl;
 
