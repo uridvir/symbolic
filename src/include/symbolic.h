ffddb0d15f0663ccad936d3048b19bae8559120f
Uri Dvir <uri.j.dvir@gmail.com>
2023-03-12 17:17:33 -0400

    Function works with addition

--- src/include/symbolic.h
+++ src/include/symbolic.h
@@ -1,8 +1,11 @@
+#include <limits>
+
 namespace symbolic {
 
 class Symbol {};
 
 enum FunctionType {
+    Null,
     Identity,
     Constant,
     Sum,
@@ -22,92 +25,69 @@ struct FunctionNode {
     bool has_children;
 };
 
-template<int N>
+const int NODES_MAX = 1024;
+
 class Function {
-    // private:
-    // const FunctionType ft;
-    // const Function& lhs;
-    // const float C;
-    // const Function& rhs;
-
-    // constexpr Function(const FunctionType ft, const Function& lhs, const float C, const Function& rhs)
-    //     : ft(ft), lhs(lhs), C(C), rhs(rhs) {};
-
-    // public:
-    // constexpr float operator()(const float& x) const {
-    //     switch(ft){
-    //         case Identity: return x;
-    //         case Constant: return C;
-    //         case Sum: return lhs(x) + rhs(x);
-    //         default: return -9000;
-    //     }
-    // };
-    // constexpr Function(const float x) : Function(Constant, *this, x, *this) {}
-    // constexpr Function(const Symbol& s) : Function(Identity, *this, 0, *this) {}
-
     private:
-    FunctionNode nodes[N];
+    const int N;
+    FunctionNode nodes[NODES_MAX];
 
-    template<int N1, int N2, int N = N1 + N2 + 1>
-    constexpr Function(FunctionType ft, const Function<N1>* lhs, const Function<N2>* rhs) : nodes() {
-        nodes[0].ft = ft;
-        
-        auto moveNodes = [this](int start, int len, FunctionNode nodes2[]) {
-            for (int i = 0; i < len; i++){
-                nodes[i + start].ft = nodes2[i].ft;
-                if (nodes[i + start].has_children = nodes2[i].has_children) {
-                    nodes[i + start].children[0] = nodes2[i].children[0] + start;
-                    nodes[i + start].children[1] = nodes2[i].children[1] + start;
-                }
+    constexpr void moveNodes(const int start, const int len, const FunctionNode nodes2[]){
+        for (int i = 0; i < len; i++){
+            nodes[i + start] = nodes2[i];
+            if (nodes[i + start].has_children) {
+                nodes[i + start].children[0] += start;
+                nodes[i + start].children[1] += start;
             }
-        };
+        }
+    }
 
+    constexpr Function(const FunctionType ft, const Function& lhs, const Function& rhs) : N(lhs.N + rhs.N + 1), nodes() {
+        nodes[0].ft = ft;
         nodes[0].children[0] = 1;
-        moveNodes(1, N1, lhs->nodes);
-        nodes[0].children[1] = 1 + N1;
-        moveNodes(1 + N1, N2, rhs->nodes);
+        moveNodes(1, lhs.N, lhs.nodes);
+        nodes[0].children[1] = 1 + lhs.N;
+        moveNodes(1 + lhs.N, rhs.N, rhs.nodes);
+        nodes[0].has_children = true;
     }
 
     constexpr float evaluateNode(const int i, const float x) const {
         switch(nodes[i].ft){
-            case Identity: 
+            case Identity:
                 return x;
             case Constant:
                 return nodes[i].C;
             case Sum: 
                 return evaluateNode(nodes[i].children[0], x) + evaluateNode(nodes[i].children[1], x);
             default:
-                std::cout << "Bad ft! " << nodes[i].ft << std::endl;
-                return -9000;
+                return std::numeric_limits<float>::quiet_NaN();
         }
     }
 
     public:
     constexpr float operator()(const float x) const { return evaluateNode(0, x); }
 
-    template<int N = 1>
-    constexpr Function(const Symbol& sym) : nodes() {
+    constexpr Function(const Symbol& sym) : N(1), nodes() {
         nodes[0].ft = Identity;
         nodes[0].sym = &sym;
         nodes[0].has_children = false;
     }
 
-    template<int N = 1>
-    constexpr Function(const float x) : nodes() {
+    constexpr Function(const float x) : N(1), nodes() {
         nodes[0].ft = Constant;
         nodes[0].C = x;
         nodes[0].has_children = false;
     }
     
-    // template<class T1, class T2>
-    // friend constexpr auto operator+(const T1& t1, const T2& t2);
+    template<class T1, class T2>
+    friend constexpr Function operator+(const T1& t1, const T2& t2);
 };
 
-// template<class T1, class T2>
-// constexpr auto operator+(const T1& t1, const T2& t2){
-//     auto f1 = t1;
-//     auto f2 = t2;
-//     return Function(Sum, f1, f2);
-// }
+template<class T1, class T2>
+constexpr Function operator+(const T1& t1, const T2& t2){
+    auto f1 = t1;
+    auto f2 = t2;
+    return Function(Sum, f1, f2);
+}
 
 }
--- test/RealFunctionTest.cpp
+++ test/RealFunctionTest.cpp
@@ -7,9 +7,7 @@ int main(int argc, char* argv[]){
 
     Symbol x;
 
-    Function<2> f = x;
-    Function<2> g = 2;
-    // Function<3> h = x + 2;
+    Function f = x + x + 2;
 
     std::cout << f(4) << std::endl;
 
