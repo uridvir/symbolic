72b0dc7421a38259f1a0a2963ed49d3458b5b888
Uri Dvir <uri.j.dvir@gmail.com>
2023-03-12 13:23:50 -0400

    operator+ is now templated

--- src/include/symbolic.h
+++ src/include/symbolic.h
@@ -33,23 +33,33 @@ class Function {
     constexpr Function(const float x) : Function(Constant, *this, x, *this) {}
     constexpr Function(const Symbol& s) : Function(Identity, *this, 0, *this) {}
     
-    constexpr Function operator+(const float& x) { //TODO: Addition operators are letting the new children die, FIX THIS
-        Function f = x;
-        return *this + f;
-    }
-    constexpr Function operator+(const Symbol& s) { //TODO: ...
-        Function f = s;
-        return *this + f;
-    }
-    constexpr Function operator+(const Function& f) {
-        return Function(Sum, *this, 0, f);
-    }
+    // constexpr Function operator+(const float& x) { //TODO: Addition operators are letting the new children die, FIX THIS
+    //     Function f = x;
+    //     return *this + f;
+    // }
+    // constexpr Function operator+(const Symbol& s) { //TODO: ...
+    //     Function f = s;
+    //     return *this + f;
+    // }
+    // constexpr Function operator+(const Function& f) {
+    //     return Function(Sum, *this, 0, f);
+    // }
+
+    // template<class T1, class T2>
+    // friend constexpr Function operator+(const T1& t1, const T2& t2){
+    //     Function f1 = t1;
+    //     Function f2 = t2;
+    //     return Function(Sum, f1, 0, f2);
+    // }
+    template<class T1, class T2>
+    friend constexpr Function operator+(const T1& t1, const T2& t2);
 };
 
-constexpr Function operator+(const Symbol& s, const float x){
-    Function f1 = s;
-    Function f2 = x;
-    return f1 + f2;
+template<class T1, class T2>
+constexpr Function operator+(const T1& t1, const T2& t2){
+    Function f1 = t1;
+    Function f2 = t2;
+    return Function(Sum, f1, 0, f2);
 }
 
 }
